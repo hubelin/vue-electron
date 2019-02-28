420bbf61b6fdfe81b39f0a0b6f87229657d90901
Hubert <huberthflin@gmail.com>
2019-02-27 19:13:21 -0500

    first commit - electron-vue test app

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <form @submit.prevent="addTask">
+      <input v-model="task" type="text">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      task: null
+    };
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    }
+  },
+  methods: {
+    addTask() {
+      this.$store.commit('addTask', this.task);
+      this.tasks = null;
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h3>Task manager</h3>
+    <!-- <form @submit.prevent="addTask">
+      <input v-model="task" type="text" />
+      <button type="submit">Submit</button>
+    </form>-->
+    <hr />
+    <h3>Tasks</h3>
+    <ul>
+      <li v-for="(item, index) in tasks" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      task: null
+    };
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    }
+  },
+  methods: {
+    addTask() {
+      this.$store.commit('addTask', this.task);
+      this.tasks = null;
+    }
+  }
+};
+</script>
